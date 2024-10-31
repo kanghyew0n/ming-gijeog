cfb4a88cabb76d5bb037fa7a6af78f402ef4417c
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-10-31 23:58:34 +0900

    🚧 Wip: css 제너레이터 생성중

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,10 +1,12 @@
 import esbuild from "esbuild";
 import pkg from "./package.json" assert { type: "json" };
 
-const dev = process.argv.includes("--dev");
+const dev = process.argv.includes("--dev"); // 개발 환경일 경우 minify 하지않음
 const minify = !dev;
 
 const watch = process.argv.includes("--watch");
+
+// package.json 에 있는 디펜던시 가져오기
 const external = Object.keys({
   ...pkg.devDependencies,
   ...pkg.peerDependencies,
@@ -20,21 +22,24 @@ const baseConfig = {
   watch,
   external,
 };
+
+// 빌드 병렬적으로 실행해줌
 Promise.all([
+  // es module 대응
   esbuild.build({
     ...baseConfig,
     format: "esm",
   }),
 
-  // commonjs es module
+  // commonjs 대응
   esbuild.build({
     ...baseConfig,
     format: "cjs",
     outExtension: {
-      ".js": ".cjs",
+      ".js": ".cjs", // commonjs 제공
     },
   }),
 ]).catch(() => {
   console.error("build failed");
-  process.exit(1);
+  process.exit(1); // 에러 났을 경우 프로젝트 종료
 });
--- packages/themes/src/variables/colors/index.ts
+++ packages/themes/src/variables/colors/index.ts
@@ -1,1 +1,1 @@
-export * as $static from "./static";
+export * as $static from "./static"; // static 예약어라 $ 구분자 추가
