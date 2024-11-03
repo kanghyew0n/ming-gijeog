2f4ce90e2910dc317ab25cb4cfede4974acba195
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-11-03 22:03:48 +0900

    ✨Feat: package 다운로드 후 사용해보기, file형식으로 다운로드 (file:)

---
+++ services/test/src/App.tsx
@@ -0,0 +1,46 @@
+import logo from "./logo.svg";
+import { ThemeProvider } from "@emotion/react";
+import "./App.css";
+import { vars } from "@kanghyewon/themes";
+import styled from "@emotion/styled";
+
+function App() {
+  const theme = {
+    colors: vars.colors.$static.light,
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <View />
+    </ThemeProvider>
+  );
+}
+
+const View = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Text>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </Text>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+/* color: ${({theme}) => theme.colors.gray[300]}; // 타입 정의가 필요할듯 */
+
+const Text = styled.p`
+color:${vars.colors.$static.light.gray[300]}; // theme 말고 바로 사용도 가능함
+`;
+
+export default App;
