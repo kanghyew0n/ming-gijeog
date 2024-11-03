de057c94d8a65c7bc2ee592fd4486df60116a599
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-11-03 22:37:19 +0900

    ✨Feat: 다크 모드 대응

--- packages/themes/scripts/build-css-module.js
+++ packages/themes/scripts/build-css-module.js
@@ -37,6 +37,26 @@ const generateThemeCssVariables = () => {
             // 자동 생성 파일이기 때문에 주석으로 표시해둠
           cssString.push(`/*자동으로 생성되는 파일입니다! 변경 금지*/ \n ${selector} {\n${cssVariables}\n}`);
         }
+
+        if (colorKey === "dark") {
+          const selector = ":root .theme-dark";
+
+          const cssVariables = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(
+                      subKey
+                    )}: ${subValue};`
+                )
+                .join("\n")
+            )
+            .join("\n"); // array들을 줄바꿈해서 합쳐줌
+
+            // 자동 생성 파일이기 때문에 주석으로 표시해둠
+          cssString.push(`/*자동으로 생성되는 파일입니다! 변경 금지*/ \n ${selector} {\n${cssVariables}\n}`);
+        }
       });
     }
   });
--- packages/themes/src/variables/colors/index.ts
+++ packages/themes/src/variables/colors/index.ts
@@ -1,1 +1,2 @@
 export * as $static from "./static"; // static 예약어라 $ 구분자 추가
+export * as $scale from "./scale";
