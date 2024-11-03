813c0a04449f4d0204a344378800eefc975a981b
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-11-03 22:03:09 +0900

    🐛 Fix:build 시 css 파일도 포함되도록 수정

--- packages/themes/scripts/build-css-module.js
+++ packages/themes/scripts/build-css-module.js
@@ -1,7 +1,7 @@
 import * as theme from "../dist/index.js";
 import fs from "fs";
 
-// theme.css
+// theme.css 이런식으로 작업되어 나타나도록 제너레이팅 함
 // :root {
 //   --gray-900: #171923
 // }
@@ -15,7 +15,6 @@ const toCssCasting = (str) => {
 
 const generateThemeCssVariables = () => {
   const cssString = [];
-
   Object.entries(theme.vars).forEach(([key, value]) => {
     if (key === "colors") {
       Object.entries(value.$static).forEach(([colorKey, colorValue]) => {
@@ -33,15 +32,13 @@ const generateThemeCssVariables = () => {
                 )
                 .join("\n")
             )
-            .join("\n");
+            .join("\n"); // array들을 줄바꿈해서 합쳐줌
 
-          return cssString.push(`${selector} {\n${cssVariables}\n}`);
+            // 자동 생성 파일이기 때문에 주석으로 표시해둠
+          cssString.push(`/*자동으로 생성되는 파일입니다! 변경 금지*/ \n ${selector} {\n${cssVariables}\n}`);
         }
       });
-
-      return;
     }
-
   });
   return cssString;
 };
@@ -49,7 +46,7 @@ const generateThemeCssVariables = () => {
 const generateThemeCss = () => {
   const variables = generateThemeCssVariables();
 
-  fs.writeFileSync("dist/themes.css", [...variables].join("\n"));
+  fs.writeFileSync("dist/themes.css", [...variables].join("\n")); // dist 파일의 themes.css는 자동으로 만들어지는 파일!!
 };
 
 generateThemeCss();
