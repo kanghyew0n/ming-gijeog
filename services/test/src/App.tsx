88e926bcc9272f234ecd10d74af8cbc6424463b0
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-11-05 23:24:26 +0900

    🚧 Wip: 패키지에서 사용해보기

--- services/test/src/App.tsx
+++ services/test/src/App.tsx
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import { ThemeProvider } from "@emotion/react";
 import "./App.css";
-import { vars } from "@kanghyewon/themes";
+import { vars, classes } from "@kanghyewon/themes";
 import styled from "@emotion/styled";
 
 function App() {
+  console.log(vars.box.shadows);
+
   const theme = {
     colors: vars.colors.$static.light,
   };
@@ -40,7 +42,9 @@ const View = () => {
 /* color: ${({theme}) => theme.colors.gray[300]}; // 타입 정의가 필요할듯 */
 
 const Text = styled.p`
-color:${vars.colors.$static.light.gray[300]}; // theme 말고 바로 사용도 가능함
+  ${classes.typography.heading["4xl"]};
+  color: ${vars.colors.$static.light
+    .gray[300]}; // theme 말고 바로 사용도 가능함
 `;
 
 export default App;
